6b8237092106c375a1061ca17d4097f80048ca76
ruben <r.alejandro.gn@gmail.com>
2024-02-26 20:24:13 -0300

    Base Parcial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Card } from './Components/Card';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Carga de estudiantes</h1>
+        <form action=''></form>
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
